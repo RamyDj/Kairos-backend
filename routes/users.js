867dcfb7ec8fc31409c85b414e12233d7b52d57e
Irene <gallibour.irene@gmail.com>
2024-08-07 15:32:01 +0200

    irene : BACK  ajout toute les route modif user et FRONT modif login

--- routes/users.js
+++ routes/users.js
@@ -13,8 +13,11 @@ const { checkBody } = require('../modules/checkBody');
 
 const ourEmail = process.env.EMAIL;
 const ourPassword = process.env.EMAIL_PASSWORD
+const urlFront = process.env.URL_FRONT  //3001
+const urlBack = process.env.URL_BACK  //3000
+const emailSecret = process.env.EMAIL_SECRET
 
-const EMAIL_SECRET = 'asdf1093KMnzxcvnkljvasdu09123nlasdasdf';
+
 
 /* router.get('/mail', (req, res) => {
   const transporter = nodemailer.createTransport({
@@ -94,11 +97,11 @@ router.post('/signup', (req, res) => {
     }, EMAIL_SECRET, { expiresIn: '1h' });
 
     // URL ROUTE GET POUR CONFIRMER MAIL
-    const url = `http://localhost:3000/users/confirmation/${emailToken}`
+    const url = `${urlBack}/users/confirmation/${emailToken}`
 
     // MAIL ENVOYE
     const mailOptions = {
-      from: "your_email@gmail.com",
+      from: ourEmail,
       to: newUser.email,
       subject: "KAIROS - Confirmation",
       text: `Click the link to confirm : <a href=${url}> ${url} </a>`,
@@ -137,7 +140,7 @@ router.post('/signup', (req, res) => {
 router.get('/confirmation/:token', (req, res) => {
 
   // Récupérer l'ID du User avec jwt
-  const user = jwt.verify(req.params.token, EMAIL_SECRET);
+  const user = jwt.verify(req.params.token, emailSecret);
   const userId = user.userId;
   console.log(userId)
 
@@ -156,7 +159,7 @@ router.get('/confirmation/:token', (req, res) => {
     User.findById(userId)
     .then(data => {
       if (data.verified === true) {
-      res.redirect('http://localhost:3001/mailconfirm')
+      res.redirect(`${urlFront}/mailconfirm`)
       /* const token = uid2(32);
       res.json({result: true, user: data, token}) */
       }
@@ -176,8 +179,7 @@ router.get('/token', (req, res) => {
   res.json({token})
 })
 
-
-// ROUTE SIGNUP
+// ROUTE SIGNUP AVANT VALIDATION EMAIL
 /* router.post('/signup', (req, res) => {
 	if (!checkBody(req.body, ['email', 'password'])) {
     res.json({ result: false, error: 'Missing or empty fields' });
@@ -230,6 +232,17 @@ router.post('/signin', (req, res) => {
 
 });
 
+// ROUTE CONNEXION GOOGLE
+router.get('/auth/google',
+  passport.authenticate('google', { scope: ['profile', 'email'] }));
+
+router.get('/auth/google/callback', 
+  passport.authenticate('google', { failureRedirect: `${urlFront}/` }),
+  function(req, res) {
+    // Successful authentication, redirect home.
+    res.redirect(`${urlFront}/dashboard`);
+  });
+
 // ROUTE DELETE USER ACCOUNT
 router.delete('/', (req, res) => {
   User.deleteOne({email: req.body.email})
@@ -255,14 +268,142 @@ router.put('/password', (req, res) => {
   })
 })
 
-router.get('/auth/google',
-  passport.authenticate('google', { scope: ['profile', 'email'] }));
+//ROUTE CHANGE NAME
+router.put('/name', (req,res) => {
+  User.findOne({email: req.body.email})
+  .then(data => {
+    if(data){   
+      User.updateOne(
+      { email: req.body.email },
+      { name: req.body.name }
+    )
+    .then(() => {
+      res.json({result: true})
+    })
+    }
+    else {
+    res.json({result: false, error: 'Could not verify user'})
+    }
+  })
+})
+//ROUTE CHANGE FIRSTNAME
+router.put('/firstname', (req,res) => {
+  User.findOne({email: req.body.email})
+  .then(data => {
+    if(data){   
+      User.updateOne(
+      { email: req.body.email },
+      { firstname: req.body.firstname }
+    )
+    .then(() => {
+      res.json({result: true})
+    })
+    }
+    else {
+    res.json({result: false, error: 'Could not verify user'})
+    }
+  })
+})
 
-router.get('/auth/google/callback', 
-  passport.authenticate('google', { failureRedirect: 'http://localhost:3001/' }),
-  function(req, res) {
-    // Successful authentication, redirect home.
-    res.redirect('http://localhost:3001/dashboard');
+//ROUTE CHANGE EMAIL AVEC VERIFICATION MAIL
+router.put('/email', (req,res) => {
+  User.findOne({ email: req.body.email })
+    .then(data => {
+      if (!data) {
+        return res.json({ result: false, error: 'Could not verify user' });
+      }
+      
+      // SETUP COMPTE ENVOI DES MAILS CONFIRMATION
+      const transporter = nodemailer.createTransport({
+      service: "Gmail",
+      host: "smtp.gmail.com",
+      port: 465,
+      secure: true,
+      auth: {
+        user: ourEmail,
+        pass: ourPassword,
+      },
+    });
+      // SETUP TOKEN A ENVOYER AU USER 
+      const emailToken = jwt.sign({userId: data._id, newEmail:req.body.newEmail}, emailSecret, { expiresIn: '1h' });
+      
+      // URL ROUTE GET POUR CONFIRMER MAIL
+      const url = `${urlBack}/users/new-email-confirmation/${emailToken}`;
+
+      // MAIL ENVOYE
+      const mailOptions = {
+        from: ourEmail,
+        to: req.body.newEmail, //nouveau mail
+        subject: "KAIROS - Confirmation Email Change",
+        text: `Click the link to confirm : <a href=${url}> ${url} </a>`,
+      };
+
+      transporter.sendMail(mailOptions, (error, info) => {
+        if (error) {
+          console.error("Error sending email: ", error);
+        } else {
+          console.log("Email sent: ", info.response);
+        }
+    })
+  })
+})
+
+//ROUTE CONFIRMATION EMAIL POUR CHANGEMENT EMAIL
+router.get('/new-email-confirmation/:token', async (req, res) => {
+
+  // Récupérer l'ID et le nouvel email du User avec jwt
+  const user = jwt.verify(req.params.token, emailSecret);
+  const userId = user.userId;
+  const newEmail = user.newEmail;
+
+  //Modifier le champ verified du document User puis modifier email
+  const updatedUser = await User.updateOne({ _id: userId }, { verified: true })
+  
+    if (updatedUser.modifiedCount === 0) {
+      User.findById(userId).then(user => {
+        if (user) {
+          res.json({result: false, error: 'Email already verified'})
+        }
+      })
+    }else{
+      const user = await User.findById(userId);
+      if (user) {
+        const udpateEmail = await User.updateOne({ _id: userId }, { email: newEmail});
+
+        if (udpateEmail.modifiedCount !== 0) {
+            res.redirect(`${urlFront}/mailconfirm`)
+        }else{
+          res.json({ result: false, message: 'Email updated not successful' });
+        }
+        }
+    }
+})
+
+
+
+/* Fonction MaJ donnée User
+  const updateUser = (req, res, fieldToUpdate, newFieldValue) => {
+  User.findOne({ email: req.body.email })
+    .then(data => {
+      if (!data) {
+        return res.json({ result: false, error: 'Could not verify user' });
+      }
+      User.updateOne({ email: req.body.email }, { [fieldToUpdate]: newFieldValue })
+        .then(() => {
+          res.json({ result: true });
+        })
+    })
+  };
+
+  // Route MaJ du nom
+  router.put('/name', (req, res) => {
+    updateUser(req, res, 'name', req.body.name);
   });
+
+  // Route MaJ du prénom
+  router.put('/firstname', (req, res) => {
+    updateUser(req, res, 'firstname', req.body.firstname);
+  });
+*/
 
 module.exports = router;
