47c58962d2791bda6143e85d93688a7ab3d01fe3
raphaelle <raflanelle@hotmail.com>
2024-08-08 09:05:21 +0200

    backend raf

--- models/status.js
+++ models/status.js
@@ -2,14 +2,17 @@ const mongoose = require('mongoose');
 
 //clé étrangère de la collection status_infos champs status_id
 const statusSchema = mongoose.Schema({
-  name: String,
-  unemployement_allocation: Boolean,
-  daily_indemnities: Boolean,
-  acre: Boolean,
-  discharged_taxes: Boolean,
-  cotisation_percentage: String,
-  max_ca: Number,
-  employees_max: Number,
+    "name": String,
+    "associes": String,
+    "registered_capital": Boolean,
+    "responsabitlity": String,
+    "taxation_of_benefits": String,
+    "unemployement_allocation": Boolean,
+    "acre": Boolean,
+    "daily_indemnities": Boolean,
+    "discharged_taxes": Boolean,
+    "cotisation_percentage": String,
+    "max_ca": String,
 });
 
 const Status = mongoose.model('status', statusSchema);
