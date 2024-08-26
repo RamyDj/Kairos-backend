6ba303281d1c533939eb9387ef3ca2b30056b4e9
Sarah <sarah.saker@hotmail.fr>
2024-08-26 14:00:43 +0200

    Sarah : app.use cookies parser avant passeport et session

--- app.js
+++ app.js
@@ -21,22 +21,22 @@ const cors = require('cors');
 app.use(cors({
     origin:  'https://kairos-frontend-six.vercel.app',
     methods: 'GET,POST,PUT,DELETE', // méthodes HTTP autorisées
-    allowedHeaders: 'Content-Type,Authorization', // headers autorisés
+    allowedHeaders: 'Content-Type,Authorization, ', // headers autorisés
     credentials: true // Permet d'inclure les cookies dans les requêtes
   }));
 
 app.use(session({ secret: 'oui', resave: false, saveUninitialized: true,
-     cookie: {
+    cookie: {
     secure: true, // Mettre à true en production si HTTPS est activé
     httpOnly: true,
     maxAge: 3600000 // Durée de vie du cookie en millisecondes (1 heure)
   } }));
+app.use(cookieParser());
 app.use(passport.initialize());
 app.use(passport.session());
 app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
-app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
